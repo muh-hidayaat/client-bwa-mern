fb14658f66ffa684bf23ae762d1b835d9925c3fb
C:/Users/Muhammad Hidayat <muh.hidayaat24@gmail.com>
2022-11-18 01:00:05 +0800

    feat: bahasa pada fitur tanggal

--- src/components/InputDate/index.js
+++ src/components/InputDate/index.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { DateRange } from 'react-date-range';
+import * as locales from 'react-date-range/dist/locale';
 
 export default function IndexDate({ date, onChangeDate, setIsShowed }) {
   React.useEffect(() => {
@@ -28,6 +29,7 @@ export default function IndexDate({ date, onChangeDate, setIsShowed }) {
       ref={refDate}
     >
       <DateRange
+        locale={locales['id']}
         editableDateInputs={true}
         onChange={onChangeDate}
         moveRangeOnFirstSelection={false}
